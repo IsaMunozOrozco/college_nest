3f3c6493a3baa704e237ed5b0cdd60b89bad854d
Isabel Munoz Orozco <108825312+IsaMunozOrozco@users.noreply.github.com>
2025-03-22 14:17:54 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,1 @@
-function showContent(topic) {
-    // Hide all topics
-    let topics = document.querySelectorAll('.content-section');
-    topics.forEach(function (section) {
-        section.style.display = 'none';
-    });
 
-    // Show the selected topic
-    let selectedTopic = document.getElementById(topic);
-    if (selectedTopic) {
-        selectedTopic.style.display = 'block';
-    }
-}
